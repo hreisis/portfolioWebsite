c7718a7ad6f88c0da23c63d6e38ef9f5376d9954
Charlene Chen <jxianing@gmail.com>
2022-07-06 20:19:02 -0700

    Added web & digital two index and contact page

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+
+//let linkButton = document.getElementById("linkButton");
+
+//linkButton.addEventListener("mouseover", function(event){
+    //event.target.style.color = "black";
+    //setTimeout(function() {
+        //event.target.style.color = "";
+      //}, 1000);
+    //}, false);
